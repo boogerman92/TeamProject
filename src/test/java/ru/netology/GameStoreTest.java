ea0622c1dd00812bdeaff2a038b8b29bba293645
Nazarenko A A <boogerman1992@gmail.com>
2023-03-15 16:10:46 +0400

    commit GameStore

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -14,5 +14,17 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
+    @Test
+    public void shouldAddsGame() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Warcraft", "Strategy");
+        Game game3 = store.publishGame("God of war", "action");
+
+        //assertTrue(store.containsGame(game1));
+        //assertTrue(store.containsGame(game2));
+        assertTrue(store.containsGame(game3));
+    }
     // другие ваши тесты
 }
