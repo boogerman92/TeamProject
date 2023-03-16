057f1286ed95dcf315bbdc2cb0305b88ba5153ea
Sapfirka <sapfirka@mail.ru>
2023-03-17 00:32:51 +0300

    game store modified addPlayTime и getSumPlayedTime

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -26,5 +26,30 @@ public class GameStoreTest {
        // assertTrue(store.containsGame(game2));
         assertTrue(store.containsGame(game3));
     }
+    @Test
+    public void shouldNoGame() {
+
+
+        GameStore store = new GameStore();
+
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Warcraft", "Strategy");
+        Game game3 = store.publishGame("God of war", "action");
+
+        assertFalse(store.containsGame(new Game("Warzone", "Shooter", store)));
+    }
+
+    @Test
+    void testAddPlayTime() {
+
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        player.installGame(game1);
+
+        store.addPlayTime("Petya", 2);
+        store.addPlayTime("Petya", 4);
+        assertEquals(6, store.getSumPlayedTime());
+    }
     // другие ваши тесты
 }
